5927d5c2d7731df293f5cba504cfd16aec037bd6
zgrkbr <ozgurkibar78@gmail.com>
2022-01-23 11:58:16 +0100

    create service interface with getStudents method

---
+++ sti-depinj-facit-domain/src/main/java/sti/depinj/facit/domain/Student.java
@@ -0,0 +1,13 @@
+package sti.depinj.facit.domain;
+
+public class Student {
+
+    private String givenName;
+
+    private String surName;
+
+    public Student(String givenName, String surName) {
+        this.givenName = givenName;
+        this.surName = surName;
+    }
+}
---
+++ sti-depinj-facit-service/src/main/java/sti/depinj/facit/service/FacitService.java
@@ -0,0 +1,11 @@
+package sti.depinj.facit.service;
+
+import sti.depinj.facit.domain.Student;
+
+import java.util.List;
+
+public interface FacitService {
+
+    List<Student> getStudents();
+
+}

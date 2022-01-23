d5337452e30e3c6dba994d2c293f54c134c0cecf
zgrkbr <ozgurkibar78@gmail.com>
2022-01-23 12:38:33 +0100

    add dep to service-impl module

--- sti-depinj-facit-unittest/src/test/java/sti/depinj/facit/unittest/TestService.java
+++ sti-depinj-facit-unittest/src/test/java/sti/depinj/facit/unittest/TestService.java
@@ -1,10 +1,14 @@
 package sti.depinj.facit.unittest;
 
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import sti.depinj.facit.service.FacitService;
 
 public class TestService {
 
     ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:sti-depinj-facit-service.xml");
 
+    FacitService facitService = (FacitService) applicationContext.getBean("facitService");
+    
 }

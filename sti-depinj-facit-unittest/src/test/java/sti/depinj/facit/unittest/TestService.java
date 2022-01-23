5ed73477ee71cff516214c329407bc2814fedc48
zgrkbr <ozgurkibar78@gmail.com>
2022-01-23 12:43:45 +0100

    test that ServiceImplFullName has been wired with FacitService

--- sti-depinj-facit-unittest/src/test/java/sti/depinj/facit/unittest/TestService.java
+++ sti-depinj-facit-unittest/src/test/java/sti/depinj/facit/unittest/TestService.java
@@ -1,14 +1,24 @@
 package sti.depinj.facit.unittest;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import sti.depinj.facit.service.FacitService;
 
+import java.util.List;
+
 public class TestService {
+
+    private static final String NASIR_TEDROS = "Nasir Tedros";
 
     ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:sti-depinj-facit-service.xml");
 
     FacitService facitService = (FacitService) applicationContext.getBean("facitService");
-    
+
+    @Test
+    public void testServiceDependencyInjection(){
+        List<String> studentNames = facitService.getStudents();
+        Assert.assertEquals(NASIR_TEDROS, studentNames.get(0));
+    }
 }
